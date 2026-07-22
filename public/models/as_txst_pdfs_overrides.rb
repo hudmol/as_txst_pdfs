cde42be9f9e042f71e90a8e4e1f3d5dbed3c1e84
James Bullen <jambun@gmail.com>
2026-07-22 16:38:03 +1000

    initial commit of txst version

--- public/models/as_txst_pdfs_overrides.rb
+++ public/models/as_txst_pdfs_overrides.rb
@@ -1,4 +1,3 @@
-
 # patch XMLCleaner to handle pagination descriptions in McCarthy like this:
 #   237&238;
 
@@ -27,102 +26,116 @@ end
 
 class FindingAidPDF
 
-  def source_file
-    # We'll use the original controller so we can find and render the PDF
-    # partials, but just for its ERB rendering.
-    renderer = PdfController.new
-    start_time = Time.now
+def source_file
+  # We'll use the original controller to render PDF partials
+  renderer = PdfController.new
+  start_time = Time.now
 
-    @repo_code = @resource.repository_information.fetch('top').fetch('repo_code')
+  @repo_code = @resource.repository_information&.fetch('top', {})&.fetch('repo_code', 'UNKNOWN')
 
-    # .length == 1 would be just the resource itself.
-    has_children = @ordered_records.entries.length > 1
+  # Check if resource has children
+  has_children = @ordered_records.entries.length > 1
 
-    out_html = Tempfile.new
-    out_html.write(renderer.render_to_string partial: 'header', layout: false, :locals => {:record => @resource})
+  out_html = Tempfile.new
+  out_html.write(renderer.render_to_string(partial: 'header', layout: false, locals: { record: @resource }))
+  out_html.write(renderer.render_to_string(partial: 'titlepage', layout: false, locals: { record: @resource }))
 
-    out_html.write(renderer.render_to_string partial: 'titlepage', layout: false, :locals => {:record => @resource})
+  # Filter archival objects for TOC
+  series_count = 0
+  toc_aos = @ordered_records.entries.drop(1).select do |entry|
+    if entry.depth == 1
+      DEPTH_1_LEVELS.include?(entry.level)
+    elsif entry.depth == 2
+      DEPTH_2_LEVELS.include?(entry.level)
+    else
+      false
+    end
+  end.map do |entry|
+    if entry.level == 'series'
+      series_count += 1
+      entry.display_string = 'Series ' + romanize(series_count) + ': ' + (entry.display_string || '')
+    end
+    entry
+  end
 
-    # Drop the resource and filter the AOs
-    series_count = 0;
-    toc_aos = @ordered_records.entries.drop(1).select {|entry|
-      if entry.depth == 1
-        DEPTH_1_LEVELS.include?(entry.level)
-      elsif entry.depth == 2
-        DEPTH_2_LEVELS.include?(entry.level)
-      else
-        false
-      end
-    }.map {|entry|
-      if entry.level == 'series'
-        series_count += 1
-        entry.display_string = 'Series ' + romanize(series_count) + ': ' +  entry.display_string
-      end
-      entry
-    }
+  out_html.write(renderer.render_to_string(
+    partial: 'toc',
+    layout: false,
+    locals: { resource: @resource, has_children: has_children, ordered_aos: toc_aos }
+  ))
 
-    out_html.write(renderer.render_to_string partial: 'toc', layout: false, :locals => {:resource => @resource, :has_children => has_children, :ordered_aos => toc_aos})
+  out_html.write(renderer.render_to_string(
+    partial: 'resource',
+    layout: false,
+    locals: { record: @resource, has_children: has_children }
+  ))
 
-    out_html.write(renderer.render_to_string partial: 'resource', layout: false, :locals => {:record => @resource, :has_children => has_children})
+  page_size = 50
+  depth_counts = {}
+  series_count = 0
+  found_instance = false
+  last_indicator = nil
 
-    page_size = 50
-    depth_counts = {}
-    series_count = 0
-    found_instance = false
-    last_indicator = false
-
-    @ordered_records.entries.drop(1).each_slice(page_size) do |entry_set|
-      if AppConfig[:pui_pdf_timeout] && AppConfig[:pui_pdf_timeout] > 0 && (Time.now.to_i - start_time.to_i) >= AppConfig[:pui_pdf_timeout]
-        raise TimeoutError.new("PDF generation timed out.  Sorry!")
-      end
-
-      uri_set = entry_set.map(&:uri)
-      record_set = archivesspace.search_records(uri_set, {}, true).records
-
-      record_set.zip(entry_set).each do |record, entry|
-        next unless record.is_a?(ArchivalObject)
-
-        new_box = false
-
-        if !Array(record.instances).empty?
-          first_container = Array(record.instances).select{|i| i.has_key?('sub_container')}.first
-          if found_instance
-            first_instance = false
-          else
-            if first_container
-              first_instance = true
-              found_instance = true
-            end
-          end
-
-          if first_container
-            if last_indicator != first_container['sub_container']['top_container']['_resolved']['indicator']
-              last_indicator = first_container['sub_container']['top_container']['_resolved']['indicator']
-              new_box = true
-            end
-          end
-        end
-
-        depth_counts[entry.depth] ||= 0
-        depth_counts[entry.depth] += 1
-        series_count += 1 if record.level == 'series'
-        out_html.write(renderer.render_to_string partial: 'archival_object', layout: false,
-                       :locals => {
-                         :record => record,
-                         :level => entry.depth,
-                         :position => depth_counts[entry.depth],
-                         :roman_position => romanize(series_count),
-                         :first_instance => first_instance,
-                         :new_box => new_box,
-                       })
-      end
+  @ordered_records.entries.drop(1).each_slice(page_size) do |entry_set|
+    if AppConfig[:pui_pdf_timeout] && AppConfig[:pui_pdf_timeout] > 0 &&
+       (Time.now.to_i - start_time.to_i) >= AppConfig[:pui_pdf_timeout]
+      raise TimeoutError.new("PDF generation timed out.  Sorry!")
     end
 
-    out_html.write(renderer.render_to_string partial: 'footer', layout: false, :locals => {:record => @resource, :has_children => has_children})
-    out_html.close
+    uri_set = entry_set.map(&:uri)
+    record_set = archivesspace.search_records(uri_set, {}, true).records
 
-    out_html
+    record_set.zip(entry_set).each do |record, entry, is_parent|
+      next unless record.is_a?(ArchivalObject)
+
+      new_box = false
+      first_instance = false
+
+      instances = Array(record.instances)
+      unless instances.empty?
+        first_container = instances.find { |i| i.key?('sub_container') }
+
+        if first_container
+          first_instance = !found_instance
+          found_instance = true if first_instance
+
+          indicator = first_container.dig('sub_container', 'top_container', '_resolved', 'indicator')
+          if indicator && last_indicator != indicator
+            last_indicator = indicator
+            new_box = true
+          end
+        end
+      end
+
+      depth_counts[entry.depth] ||= 0
+      depth_counts[entry.depth] += 1
+      series_count += 1 if record.level == 'series'
+
+      out_html.write(renderer.render_to_string(
+        partial: 'archival_object',
+        layout: false,
+        locals: {
+          record: record,
+          level: entry.depth,
+          position: depth_counts[entry.depth],
+          roman_position: romanize(series_count),
+          first_instance: first_instance,
+          new_box: new_box,
+          is_parent: is_parent
+        }
+      ))
+    end
   end
+
+  out_html.write(renderer.render_to_string(
+    partial: 'footer',
+    layout: false,
+    locals: { record: @resource, has_children: has_children }
+  ))
+
+  out_html.close
+  out_html
+end
 
   def romanize(n)
     # sigh
